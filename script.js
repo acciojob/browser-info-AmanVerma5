afe77b5c74bc31b807671a2dd94646b6aa1f8667
AmanVerma5 <118344057+AmanVerma5@users.noreply.github.com>
2023-06-06 14:17:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 //your JS code here. If required.
 let div=document.getElementById("browser-info");
 let info=navigator.userAgent;
-div.innerText+=` ${info}`;
+let version=navigator.appVersion;
+
+function getName(info){
+	if (info.includes("Firefox")) {
+    return "Firefox";
+  } else if (info.includes("Chrome")) {
+    return "Chrome";
+  } else if (info.includes("Safari")) {
+    return "Safari";
+  } else if (info.includes("Opera") || info.includes("OPR")) {
+    return "Opera";
+  } else if (info.includes("Edge")) {
+    return "Microsoft Edge";
+  } else if (info.includes("MSIE") || info.includes("Trident/")) {
+    return "Internet Explorer";
+  } else {
+    return "Unknown";
+  }
+}
+let name=getName(info);
+div.innerText+=` ${name} version ${version}`
